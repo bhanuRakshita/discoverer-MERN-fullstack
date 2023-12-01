fb77606c1ebd7f6aeb83a96d40ce1d551a5a19e5
bhanuRakshita <bhanurakshita457@gmail.com>
2023-12-01 17:03:38 -0500

    Created database for places

--- back-end/app.js
+++ back-end/app.js
@@ -1,30 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes.js');
-const usersRoutes = require('./routes/users-routes.js');
-const HttpError = require('./models/http-error.js');
+const placesRoutes = require("./routes/places-routes.js");
+const usersRoutes = require("./routes/users-routes.js");
+const HttpError = require("./models/http-error.js");
 
 const app = express();
 
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+
 app.use(bodyParser.json());
 
-app.use('/api/places',placesRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 //for unsupported routes
-app.use((req, res, next)=> {
-    const error = new HttpError('Could not find this route.', 404);
-    throw error;
-})
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
-app.use((error, req, res, next)=>{
-    if(res.headerSent){
-        return next(error);
-    }
-    res.status(error.code || 500)
-    res.json({message: error.message || 'An unkown error occured'});
-})
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unkown error occured" });
+});
 
-app.listen(3000);
+mongoose
+.connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.bgfe8le.mongodb.net/placesDB?retryWrites=true&w=majority`)
+.then(()=>{
+    app.listen(3000);
+    console.log('Successful connection to db');
+})
+.catch(err=>{
+    console.log('Could no connect to DB');
+    console.log(err)
+});
--- back-end/controllers/places-controller.js
+++ back-end/controllers/places-controller.js
@@ -1,98 +1,112 @@
-const { v4: uuidv4 } = require('uuid');
-const {validationResult} = require('express-validator');
+const { v4: uuidv4 } = require("uuid");
+const { validationResult } = require("express-validator");
 
-const HttpError = require('../models/http-error');
-const getCoordsForAddress = require('../utils/location');
+const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../utils/location");
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
-    {
-      id: 'p1',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      location: {
-        lat: 40.7484474,
-        lng: -73.9871516
-      },
-      address: '20 W 34th St, New York, NY 10001',
-      creator: 'u1'
-    }
-  ];
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
 
-const getPlaceById = (req, res, next)=> {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find((p)=>p.id===placeId);
-    console.log('GET Request in places');
-    if (!place) {
-        throw new HttpError('No place found with given place id', 404);
-    };
-    
-    res.json({place});
+const getPlaceById = async (req, res, next) => {
+  const placeId = req.params.pid;
+  let place;
+
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    // following erroris if there was a problwth get request
+    const error = new HttpError("Something went wrong, could not find a place");
+  }
+
+  if (!place) {
+    const error = new HttpError("No place found with given place id", 404);
+    return next(error);
+  }
+
+  res.json({ place: place.toObject({ getters: true }) });
 };
 
-const getPlaceByUserId = (req, res, next)=>{
-    const uid = req.params.uid;
-    const places = DUMMY_PLACES.filter((place)=>place.creator===uid);
-    if (places.length===0) {
-        return next(new HttpError('No place found with given user id', 404));
-    };
-    res.json(places);
+const getPlaceByUserId = (req, res, next) => {
+  const uid = req.params.uid;
+  const places = DUMMY_PLACES.filter((place) => place.creator === uid);
+  if (places.length === 0) {
+    return next(new HttpError("No place found with given user id", 404));
+  }
+  res.json(places);
 };
 
 const createPlace = async (req, res, next) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()){
-        next(new HttpError('Invalid input', 422)) ;
-    }
-    const { title, description, address, creator} = req.body;
-    let coordinates; 
-    
-    try {
-        coordinates = await getCoordsForAddress(address);
-    } catch(error) {
-        return next(error);
-    }
+  if (!errors.isEmpty()) {
+    next(new HttpError("Invalid input", 422));
+  }
+  const { title, description, address, creator } = req.body;
+  let coordinates;
 
-    const createdPlace = {
-        id: uuidv4(),
-        title,
-        description,
-        location: coordinates,
-        address,
-        creator
-    };
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
 
-    DUMMY_PLACES.push(createdPlace);
-    res.status(201).json({place: createdPlace});
+  const createdPlace = new Place({
+    title,
+    description,
+    address,
+    location: coordinates,
+    image:
+      "https://cdn.britannica.com/86/170586-120-7E23E561/Taj-Mahal-Agra-India.jpg",
+    creator,
+  });
+  try {
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError("Creating place failed, please try again", 500);
+    return next(error);
+  }
+  res.status(201).json({ place: createdPlace });
 };
 
 const updatePlaceById = (req, res, next) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()){
-        throw new HttpError('Invalid input', 422);
-    }
-    const pid = req.params.pid;
-    const { title, description} = req.body;
-    const updatedPlace = {...DUMMY_PLACES.find((p)=>p.id===pid)};
-    const placeIndex = DUMMY_PLACES.findIndex((p)=>p.id===pid);
-    updatedPlace.title = title;
-    updatedPlace.description = description;
+  if (!errors.isEmpty()) {
+    throw new HttpError("Invalid input", 422);
+  }
+  const pid = req.params.pid;
+  const { title, description } = req.body;
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === pid) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === pid);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
 
-    DUMMY_PLACES[placeIndex] = updatedPlace;
+  DUMMY_PLACES[placeIndex] = updatedPlace;
 
-    res.status(200).json({place: updatedPlace});
+  res.status(200).json({ place: updatedPlace });
 };
 
 const deletePlaceById = (req, res, next) => {
-    const pid = req.params.pid;
-    if (!DUMMY_PLACES.find(p=>p.id===pid)) {
-        throw new HttpError('No place found with given id', 422);
-    }
+  const pid = req.params.pid;
+  if (!DUMMY_PLACES.find((p) => p.id === pid)) {
+    throw new HttpError("No place found with given id", 422);
+  }
 
-    DUMMY_PLACES = DUMMY_PLACES.filter((place)=>place.id!==pid);
-    res.status(201).json({message: 'Deleted place', newPlaces: DUMMY_PLACES});
-}
+  DUMMY_PLACES = DUMMY_PLACES.filter((place) => place.id !== pid);
+  res.status(201).json({ message: "Deleted place", newPlaces: DUMMY_PLACES });
+};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
